74e0359f9f98836b4962f6e51b3538ef833b2b4a
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-25 20:59:23 +0900

    added blog.js

---
+++ js/blog.js
@@ -0,0 +1,19 @@
+
+let parent = document.querySelectorAll(".menu");
+let node = Array.prototype.slice.call(parent,0);
+  node.forEach(function (element) {
+      element.addEventListener(
+        "mouseover",
+        function() {
+          element.querySelector(".submenu").classList.add("acrive");
+        },
+        false
+      );
+      element.addEventListener(
+      "mouseout",
+      function () {
+      element.querySelector(".submenu").classList.remove("active");
+    },
+    false
+  );
+});

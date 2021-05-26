7eb997cfa62272f2764177fe5cff283aff7193c2
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 19:08:06 +0900

    modified js

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 
-var parent = document.querySelectorId("#menublog");
+var parent = document.querySelectorId("submenu");
 var node = Array.prototype.slice.call(parent,0);
   node.forEach(function (element) {
       element.addEventListener(

3a6edaf2185e5509cc50467ed92fbad3337c8ced
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 18:05:51 +0900

    modified css and js

--- js/blog.js
+++ js/blog.js
@@ -1,18 +1,18 @@
 
-var parent = document.querySelectorAll(".menu");
+var parent = document.querySelectorid("#menublog");
 var node = Array.prototype.slice.call(parent,0);
   node.forEach(function (element) {
       element.addEventListener(
         "mouseover",
         function() {
-          element.querySelector(".submenu").classList.add("active");
+          element.querySelector("#menublog").classList.add("active");
         },
         false
       );
       element.addEventListener(
       "mouseout",
       function () {
-      element.querySelector(".submenu").classList.remove("active");
+      element.querySelector("#menublog").classList.remove("active");
     },
     false
   );
--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,8 @@
-$(function(){
-  $("#menublog").mouseover(function(){
-    $(this).children(".submenu").stop().slideDown();
-  });
-  $("#menublog").mouseout(function(){
-    $(".submenu").stop().slideUp();
-  });
-});
+// $(function(){
+//   $("#menublog").mouseover(function(){
+//     $(this).children(".submenu").stop().slideDown();
+//   });
+//   $("#menublog").mouseout(function(){
+//     $(".submenu").stop().slideUp();
+//   });
+// });

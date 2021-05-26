a05401ac83bc16d60da7312a7cf56573d1c7e918
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 18:14:13 +0900

    modified js

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,8 @@
 $(function(){
-  $(".menu").mouseover(function(){
-    $(this).children(".submenu").stop().slideDown();
+  $("#menublog").mouseover(function(){
+    $(".submenu").stop().slideDown();
   });
-  $(".menu").mouseout(function(){
+  $("#menublog").mouseout(function(){
     $(".submenu").stop().slideUp();
   });
 });

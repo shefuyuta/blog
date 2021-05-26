6fee1fc3c9272b175e6d19fdb1c4dc9fe6e27f6d
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 22:09:27 +0900

    modified js and css

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 
-var parent = document.querySelectorId(".submenu");
+var parent = document.querySelectorAll(".hassub");
 var node = Array.prototype.slice.call(parent,0);
   node.forEach(function (element) {
       element.addEventListener(

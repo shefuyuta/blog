acea947113ddd4ca129c8934719738665f603bac
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 18:06:44 +0900

    modified js

--- js/blog.js
+++ js/blog.js
@@ -1,19 +1,19 @@
-
-var parent = document.querySelectorid("#menublog");
-var node = Array.prototype.slice.call(parent,0);
-  node.forEach(function (element) {
-      element.addEventListener(
-        "mouseover",
-        function() {
-          element.querySelector("#menublog").classList.add("active");
-        },
-        false
-      );
-      element.addEventListener(
-      "mouseout",
-      function () {
-      element.querySelector("#menublog").classList.remove("active");
-    },
-    false
-  );
-});
+//
+// var parent = document.querySelectorid("#menublog");
+// var node = Array.prototype.slice.call(parent,0);
+//   node.forEach(function (element) {
+//       element.addEventListener(
+//         "mouseover",
+//         function() {
+//           element.querySelector("#menublog").classList.add("active");
+//         },
+//         false
+//       );
+//       element.addEventListener(
+//       "mouseout",
+//       function () {
+//       element.querySelector("#menublog").classList.remove("active");
+//     },
+//     false
+//   );
+// });

3bf2bf8ee5b034f8e3bf73dafbd83e6cfe82deb7
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 19:39:30 +0900

    modified js

--- js/blog.js
+++ js/blog.js
@@ -1,19 +1,19 @@
-//
-// var parent = document.querySelectorId(".submenu");
-// var node = Array.prototype.slice.call(parent,0);
-//   node.forEach(function (element) {
-//       element.addEventListener(
-//         "mouseover",
-//         function() {
-//           element.querySelector(".submenu").classList.add("active");
-//         },
-//         false
-//       );
-//       element.addEventListener(
-//       "mouseout",
-//       function () {
-//       element.querySelector(".submenu").classList.remove("active");
-//     },
-//     false
-//   );
-// });
+
+var parent = document.querySelectorId(".submenu");
+var node = Array.prototype.slice.call(parent,0);
+  node.forEach(function (element) {
+      element.addEventListener(
+        "mouseover",
+        function() {
+          element.querySelector(".submenu").classList.add("active");
+        },
+        false
+      );
+      element.addEventListener(
+      "mouseout",
+      function () {
+      element.querySelector(".submenu").classList.remove("active");
+    },
+    false
+  );
+});

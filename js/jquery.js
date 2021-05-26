13ede399600b01c84fffa6256a240e7f40af35fc
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 22:24:46 +0900

    enabled jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,15 @@
-// $(function(){
-//   $("#menublog").mouseover(function(){
-//     $(".submenu").stop().slideDown();
-//   });
-//   $("#menublog").mouseout(function(){
-//     $(".submenu").stop().slideUp();
-//     subHeight = $(".submenu").height();
-//     $(".submenu").removeClass("active");
-//     if( subHeight <= 0 ){
-//       $(".submenu").addClass("active");
-//     }else{
-//       $(".submenu").removeClass("active");
-//     }
-//   });
-// });
+$(function(){
+  $("#menublog").mouseover(function(){
+    $(".submenu").stop().slideDown();
+  });
+  $("#menublog").mouseout(function(){
+    $(".submenu").stop().slideUp();
+    subHeight = $(".submenu").height();
+    $(".submenu").removeClass("active");
+    if( subHeight <= 0 ){
+      $(".submenu").addClass("active");
+    }else{
+      $(".submenu").removeClass("active");
+    }
+  });
+});

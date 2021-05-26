0f5be1e141da62228693e3adaa62fc97aec9011e
shefuyuta <shefuyuta@users.noreply.github.com>
2021-05-26 18:43:27 +0900

    modified js

--- js/blog.js
+++ js/blog.js
@@ -1,18 +1,18 @@
 
-var parent = document.querySelectorId("#menublog");
-var node = Array.prototype.slice.call(".submenu",0);
+var parent = document.querySelectorId(".submenu");
+var node = Array.prototype.slice.call(parent,0);
   node.forEach(function (element) {
       element.addEventListener(
         "mouseover",
         function() {
-          element.querySelector("#menublog").classList.add("active");
+          element.querySelector(".submenu").classList.add("active");
         },
         false
       );
       element.addEventListener(
       "mouseout",
       function () {
-      element.querySelector("#menublog").classList.remove("active");
+      element.querySelector(".submenu").classList.remove("active");
     },
     false
   );

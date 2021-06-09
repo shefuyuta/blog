eb772f76ac6087e6a32a859065a9469b1a44261f
shefuyuta <shefuyuta@users.noreply.github.com>
2021-06-09 20:59:31 +0900

    modified css

--- js/blog.js
+++ js/blog.js
@@ -18,3 +18,25 @@ var parent = document.querySelectorAll("#menublog");
       false
     );
   });
+
+var blogtopics = document.querySelectorAll(".blogtopics")
+var topic = Array.prototype.slice.call(parent, 0);
+
+node.forEach(function (element) {
+  element.addEventListener(
+    "mouseover",
+    function () {
+      element.querySelector(".blogtopics").classList.add("active");
+      element.querySelector(".topic").classList.add("active");
+    },
+    false
+  );
+  element.addEventListener(
+    "mouseout",
+    function () {
+      element.querySelector(".blogtopics").classList.remove("active");
+      element.querySelector(".topic").classList.remove("active");
+    },
+    false
+  );
+});
